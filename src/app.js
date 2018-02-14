9d0f3d262d0d424bc6361dde5c71dedbb0765837
igorbojk <igorbojk@gmail.com>
2018-02-14 10:10:24 +0200

    stage save

--- src/app.js
+++ src/app.js
@@ -1,14 +1,35 @@
 import Vue from 'vue';
+import VueResource from 'vue-resource';
 import Hello from './components/hello.vue';
 
+Vue.use(VueResource);
+
 new Vue({
-    data: function () {
-        return {
-            message: 'Webpack and Vue setup'
-        }
-    },
     el: '#vue-app',
     components: {
         hello: Hello,
+    },
+    data: {
+        endpoint: 'https://jsonplaceholder.typicode.com/photos',
+        params: {
+          limit: 2,
+        },
+        list: [],
+    },
+    methods: {
+        getAllPosts: function () {
+            this.$http.get(this.endpoint, this.params)
+                .then(
+                    (result) => {
+                        console.log(result);
+                        this.list = result.body
+                    }
+                ), function (error) {
+                throw new Error(error);
+            }
+        }
+    },
+    created: function () {
+        this.getAllPosts();
     }
 });
